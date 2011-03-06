c17a72a4e57d5e67b5790d1ae88af451c056fccb
Nils Hartmann <nils@nilshartmann.net>
2011-03-06 09:16:07 +0100

    minor javadoc improvements

--- org.bundlemaker.core/src/org/bundlemaker/core/projectdescription/IResourceContent.java
+++ org.bundlemaker.core/src/org/bundlemaker/core/projectdescription/IResourceContent.java
@@ -15,6 +15,14 @@ import org.eclipse.core.runtime.IPath;
  * <code>addResourceContent()</code> methods on the interface
  * {@link IBundleMakerProjectDescription}.
  * </p>
+ * <p>
+ * <p>
+ * <b>Paths and Resources</b>
+ * <p>
+ * A resource content contains paths and resources. A <b>path</b> is a a simple
+ * pointer to a location somewhere on the file system. A <b>resource</b>
+ * represents the (parsed) content, that is available from that location.
+ * </p>
  * 
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  * 
@@ -28,6 +36,10 @@ public interface IResourceContent {
 	 * Returns the set of all defined source paths for this content entry. The
 	 * result set is never <code>null</code>, but maybe empty.
 	 * </p>
+	 * 
+	 * <p>
+	 * Note: to get a list of the binary paths of this IResourceContent, use
+	 * <tt>getBinaryPaths() on its owning IFileBasedContent instance
 	 * 
 	 * @return the set of all defined source paths for this content entry.
 	 */
@@ -46,55 +58,70 @@ public interface IResourceContent {
 
 	/**
 	 * <p>
-	 * Returns the {@link IResource} with 
+	 * Returns the {@link IResource} for the specified path and type
 	 * </p>
 	 * 
 	 * @param path
+	 *            The path of the resource that should be returned
 	 * @param type
+	 *            The type of the resource, either binary or source
 	 * @return
 	 */
 	IResource getResource(IPath path, ContentType type);
 
 	/**
 	 * <p>
+	 * Returns a {@link Set} of all resources of the specified type
 	 * </p>
 	 * 
 	 * @param type
-	 * @return
+	 * @return a Set of resources, never null.
 	 */
 	Set<? extends IResource> getResources(ContentType type);
 
 	/**
 	 * <p>
+	 * Returns the binary {@link IResource resource} for the specified path
 	 * </p>
 	 * 
 	 * @param path
-	 * @return
+	 * @return the resource the path points to or null if no such resource is
+	 *         available
 	 */
 	IResource getBinaryResource(IPath path);
 
 	/**
 	 * <p>
+	 * Returns a {@link Set} of all binary resources
+	 * </p>
+	 * <p>
+	 * This is a convenience method for {@link #getResources(ContentType)
+	 * getResources(ContentType.BINARY)}
 	 * </p>
 	 * 
-	 * @return
+	 * @return a Set of resources, never null.
 	 */
 	Set<? extends IResource> getBinaryResources();
 
 	/**
 	 * <p>
+	 * Returns the {@link IResource Resource} for the specified path
 	 * </p>
 	 * 
 	 * @param path
-	 * @return
+	 * @return The resource for the given path or null if there is no resource
+	 *         for the given path
 	 */
 	IResource getSourceResource(IPath path);
 
 	/**
+	 * Returns all source resources
 	 * <p>
+	 * This is a convenience method for {@link #getResources(ContentType)
+	 * getResources(ContentType.SOURCE)}
 	 * </p>
 	 * 
-	 * @return
+	 * @return a Set of resources, never null.
 	 */
 	Set<? extends IResource> getSourceResources();
 
