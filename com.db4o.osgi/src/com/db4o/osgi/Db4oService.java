bf4a790afb99bbf45636cf811000cae79c675582
wuetherich <gerd@gerd-wuetherich.de>
2011-04-15 13:00:14 +0200

    added license agreement

--- com.db4o.osgi/src/com/db4o/osgi/Db4oService.java
+++ com.db4o.osgi/src/com/db4o/osgi/Db4oService.java
@@ -1,5 +1,11 @@
-/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
-
+/*******************************************************************************
+ * Copyright (C) 2007  db4objects Inc.  http://www.db4o.com
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the db4o Opensource Compatibility License (dOCL)
+ * which accompanies this distribution, and is available at
+ * http://www.db4o.com/about/company/legalpolicies/docl.aspx
+ *******************************************************************************/
 package com.db4o.osgi;
 
 import com.db4o.*;
