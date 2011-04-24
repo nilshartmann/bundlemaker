6d4ed5749663012aa722b2df927ad924013e7fe5
Nils Hartmann <nils@nilshartmann.net>
2011-04-24 13:30:28 +0200

    BM-90 Adding of Archives, External Archives, Folders and External Folders implemented

---
+++ org.bundlemaker.core.ui/src/org/bundlemaker/core/ui/editor/ContentListBlock.java
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Bundlemaker project team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Bundlemaker project team - initial API and implementation
+ ******************************************************************************/
+package org.bundlemaker.core.ui.editor;
+
+import static java.lang.String.format;
+
+import java.util.Arrays;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.internal.ui.wizards.TypedViewerFilter;
+import org.eclipse.jdt.internal.ui.wizards.buildpaths.MultipleFolderSelectionDialog;
+import org.eclipse.jdt.ui.wizards.BuildPathDialogAccess;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.model.WorkbenchContentProvider;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+/**
+ * A block that displays and selects archives, folders and variables
+ * 
+ * @author Nils Hartmann (nils@nilshartmann.net)
+ * 
+ */
+public class ContentListBlock {
+
+  /**
+   * The list containing the currently selected entries
+   */
+  private List _contentList;
+
+  public void createContent(Composite parent) {
+    Composite contentListComposite = new Composite(parent, SWT.NONE);
+    final Shell shell = contentListComposite.getShell();
+    GridData layoutData = new GridData(GridData.FILL_BOTH);
+    contentListComposite.setLayoutData(layoutData);
+    contentListComposite.setLayout(new GridLayout(2, false));
+
+    _contentList = new List(contentListComposite, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI);
+    layoutData = new GridData(GridData.FILL_BOTH);
+    layoutData.verticalIndent = 0;
+    _contentList.setLayoutData(layoutData);
+
+    Composite buttonBar = new Composite(contentListComposite, SWT.NONE);
+
+    buttonBar.setLayout(new GridLayout(1, false));
+    GridData gd = new GridData();
+    gd.verticalAlignment = GridData.BEGINNING;
+    gd.horizontalAlignment = GridData.FILL;
+    gd.verticalIndent = 0;
+    buttonBar.setLayoutData(gd);
+
+    newTextButton(buttonBar, "Add entry...", null);
+    newTextButton(buttonBar, "Edit entry...", null);
+
+    newTextButton(buttonBar, "Remove Entry", new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        _contentList.remove(_contentList.getSelectionIndices());
+      }
+
+    });
+
+    newTextButton(buttonBar, "Add Archives...", new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        addArchives(shell);
+      }
+
+    });
+
+    newTextButton(buttonBar, "Add external archives...", new SelectionAdapter() {
+
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        addExternalArchives(shell, "Select archives to add...");
+      }
+
+    });
+
+    newTextButton(buttonBar, "Add Folder...", new SelectionAdapter() {
+
+      /*
+       * (non-Javadoc)
+       * 
+       * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+       */
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        addFolder(shell);
+      }
+
+    });
+
+    newTextButton(buttonBar, "Add external folder...", new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+        addExternalFolder(shell);
+      }
+
+    });
+
+  }
+
+  /**
+   * Opens a {@link DirectoryDialog} and returns the qualified selected folder or null if the dialog has been canceled.
+   * 
+   * @param parentShell
+   * @return
+   */
+  private void addExternalFolder(Shell parentShell) {
+    DirectoryDialog dialog = new DirectoryDialog(parentShell, SWT.MULTI);
+    String folder = dialog.open();
+    if (folder != null) {
+      _contentList.add(folder);
+    }
+  }
+
+  private void addExternalArchives(Shell parentShell, String title) {
+    FileDialog fileDialog = new FileDialog(parentShell, SWT.MULTI);
+    fileDialog.setText(title);
+    fileDialog.setFilterExtensions(new String[] { "*.jar;*.zip", "*.*" });
+    if (fileDialog.open() == null) {
+      return;
+    }
+    String[] fileNames = fileDialog.getFileNames();
+    if (fileNames.length > 0) {
+      for (int i = 0; i < fileNames.length; i++) {
+        IPath path = new Path(fileDialog.getFilterPath()).append(fileNames[i]);
+        _contentList.add(path.toOSString());
+
+      }
+    }
+  }
+
+  private void addArchives(Shell parentShell) {
+    IPath[] selected = BuildPathDialogAccess.chooseJAREntries(parentShell, null, new IPath[0]);
+
+    if (selected == null || selected.length < 1) {
+      return;
+    }
+
+    for (int i = 0; i < selected.length; i++) {
+      String workspacePath = format("${workspace_loc:%s}", selected[i].toString());
+      _contentList.add(workspacePath);
+    }
+
+  }
+
+  @SuppressWarnings("restriction")
+  private void addFolder(Shell shell) {
+    MultipleFolderSelectionDialog dialog = new MultipleFolderSelectionDialog(shell, new WorkbenchLabelProvider(),
+        new WorkbenchContentProvider());
+    dialog.setInput(ResourcesPlugin.getWorkspace().getRoot());
+    dialog.setTitle("Select Folders");
+    dialog.setMessage("Select Folder you want to add to your resource definition");
+    dialog.addFilter(new TypedViewerFilter(new Class[] { IProject.class, IFolder.class }));
+    if (dialog.open() == Window.OK) {
+      Object[] elements = dialog.getResult();
+      for (int i = 0; i < elements.length; i++) {
+        IResource elem = (IResource) elements[i];
+        String workspaceFolder = format("${workspace_loc:%s}", elem.getFullPath());
+        _contentList.add(workspaceFolder);
+      }
+
+    }
+
+  }
+
+  private Button newTextButton(Composite composite, String text, SelectionListener listener) {
+
+    final Button button = new Button(composite, SWT.PUSH);
+    button.setText(text);
+    button.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+    if (listener != null) {
+      button.addSelectionListener(listener);
+    }
+    return button;
+
+  }
+
+  public java.util.List<String> getItems() {
+    return Arrays.asList(_contentList.getItems());
+  }
+
+  /**
+   * @param array
+   */
+  public void setItems(String[] items) {
+    _contentList.setItems(items);
+  }
+
+}
--- org.bundlemaker.core.ui/src/org/bundlemaker/core/ui/editor/ModifyProjectContentDialog.java
+++ org.bundlemaker.core.ui/src/org/bundlemaker/core/ui/editor/ModifyProjectContentDialog.java
@@ -1,28 +1,20 @@
 package org.bundlemaker.core.ui.editor;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Set;
 
 import org.bundlemaker.core.projectdescription.IFileBasedContent;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.layout.PixelConverter;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
@@ -36,9 +28,9 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
 
   private Text                   _versionTextField;
 
-  private List                   _binariesList;
+  private ContentListBlock       _binariesContentList;
 
-  private List                   _sourcesList;
+  private ContentListBlock       _sourcesContentList;
 
   private String                 _name;
 
@@ -109,9 +101,9 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
     dialogComposite.setLayout(gridLayout);
 
     addNameAndVersionRow(dialogComposite);
-    _binariesList = addContentList(dialogComposite, "Binaries");
+    _binariesContentList = addContentList(dialogComposite, "Binaries");
     if (_editResources) {
-      _sourcesList = addContentList(dialogComposite, "Sources");
+      _sourcesContentList = addContentList(dialogComposite, "Sources");
     }
 
     prepopulateForm();
@@ -130,9 +122,9 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
     _nameTextField.setText(_name);
     _versionTextField.setText(_version);
 
-    _binariesList.setItems(_binaryRoots.toArray(new String[0]));
-    if (_sourcesList != null) {
-      _sourcesList.setItems(_sourceRoots.toArray(new String[0]));
+    _binariesContentList.setItems(_binaryRoots.toArray(new String[0]));
+    if (_sourcesContentList != null) {
+      _sourcesContentList.setItems(_sourceRoots.toArray(new String[0]));
     }
   }
 
@@ -144,84 +136,12 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
     _versionTextField = addText(dialogComposite);
   }
 
-  private List addContentList(Composite dialogComposite, String title) {
+  private ContentListBlock addContentList(Composite dialogComposite, String title) {
     addLabel(dialogComposite, title);
 
-    Composite contentListComposite = new Composite(dialogComposite, SWT.NONE);
-    GridData layoutData = new GridData(GridData.FILL_BOTH);
-    contentListComposite.setLayoutData(layoutData);
-    contentListComposite.setLayout(new GridLayout(2, false));
-
-    final List list = new List(contentListComposite, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI);
-    layoutData = new GridData(GridData.FILL_BOTH);
-    layoutData.verticalIndent = 0;
-    // layoutData.verticalAlignment = GridData.BEGINNING;
-    // layoutData.grabExcessVerticalSpace = true;
-    list.setLayoutData(layoutData);
-
-    Composite buttonBar = new Composite(contentListComposite, SWT.NONE);
-
-    buttonBar.setLayout(new GridLayout(1, false));
-    GridData gd = new GridData();
-    gd.verticalAlignment = GridData.BEGINNING;
-    gd.horizontalAlignment = GridData.FILL;
-    gd.verticalIndent = 0;
-    buttonBar.setLayoutData(gd);
-
-    final Button addArchivesButton = new Button(buttonBar, SWT.PUSH);
-    addArchivesButton.setText("Add archives...");
-    addArchivesButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-    addArchivesButton.addSelectionListener(new SelectionListener() {
-
-      @Override
-      public void widgetSelected(SelectionEvent e) {
-        String[] archives = selectArchives(addArchivesButton.getShell(), "Select archives to add");
-        for (String archive : archives) {
-          list.add(archive);
-        }
-
-      }
-
-      @Override
-      public void widgetDefaultSelected(SelectionEvent e) {
-      }
-    });
-
-    final Button addFolderButton = new Button(buttonBar, SWT.PUSH);
-    addFolderButton.setText("Add folder...");
-    addFolderButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-    addFolderButton.addSelectionListener(new SelectionListener() {
-
-      @Override
-      public void widgetSelected(SelectionEvent e) {
-        String folder = selectFolder(addFolderButton.getShell());
-        if (folder != null) {
-          list.add(folder);
-        }
-      }
-
-      @Override
-      public void widgetDefaultSelected(SelectionEvent e) {
-
-      }
-    });
-    Button removeButton = new Button(buttonBar, SWT.PUSH);
-    removeButton.setText("Remove");
-    removeButton.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-    removeButton.addSelectionListener(new SelectionListener() {
-
-      @Override
-      public void widgetSelected(SelectionEvent e) {
-        list.remove(list.getSelectionIndices());
-      }
-
-      @Override
-      public void widgetDefaultSelected(SelectionEvent e) {
-      }
-    });
-
-    return list;
-
+    ContentListBlock contentListBlock = new ContentListBlock();
+    contentListBlock.createContent(dialogComposite);
+    return contentListBlock;
   }
 
   private Text addText(Composite parent) {
@@ -245,39 +165,6 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
     return label;
   }
 
-  /**
-   * Opens a {@link DirectoryDialog} and returns the qualified selected folder or null if the dialog has been canceled.
-   * 
-   * @param parentShell
-   * @return
-   */
-  private String selectFolder(Shell parentShell) {
-    DirectoryDialog dialog = new DirectoryDialog(parentShell, SWT.MULTI);
-    return dialog.open();
-  }
-
-  private String[] selectArchives(Shell parentShell, String title) {
-    FileDialog fileDialog = new FileDialog(parentShell, SWT.MULTI);
-    fileDialog.setText(title);
-    fileDialog.setFilterExtensions(new String[] { "*.jar;*.zip", "*.*" });
-    if (fileDialog.open() == null) {
-      return new String[0];
-    }
-    String[] fileNames = fileDialog.getFileNames();
-    String[] binaryRoots = new String[fileNames.length];
-    if (fileNames.length > 0) {
-      // Add all selected archives to the project description
-      for (int i = 0; i < fileNames.length; i++) {
-        IPath path = new Path(fileDialog.getFilterPath()).append(fileNames[i]);
-        String binaryRoot = path.toOSString();
-        binaryRoots[i] = binaryRoot;
-
-      }
-
-    }
-    return binaryRoots;
-  }
-
   public String getName() {
     return _name;
   }
@@ -298,10 +185,10 @@ public class ModifyProjectContentDialog extends TitleAreaDialog {
   protected void okPressed() {
     _name = _nameTextField.getText();
     _version = _versionTextField.getText();
-    _binaryRoots = Arrays.asList(_binariesList.getItems());
+    _binaryRoots = _binariesContentList.getItems();
 
-    if (_sourcesList != null) {
-      _sourceRoots = Arrays.asList(_sourcesList.getItems());
+    if (_sourcesContentList != null) {
+      _sourceRoots = _sourcesContentList.getItems();
     }
     super.okPressed();
   }
