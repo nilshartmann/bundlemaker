fd0ab154ff2b5b64c438875a02897b66beb827f4
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-05-10 09:40:10 +0200

    ~ introduced some logging

---
+++ org.bundlemaker.core/src/org/bundlemaker/core/internal/parser/LoggableAction.java
@@ -0,0 +1,18 @@
+package org.bundlemaker.core.internal.parser;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ * 
+ */
+public interface LoggableAction<T> {
+
+  /**
+   * <p>
+   * </p>
+   * 
+   */
+  public T execute();
+}
---
+++ org.bundlemaker.core/src/org/bundlemaker/core/internal/parser/StaticLog.java
@@ -0,0 +1,55 @@
+package org.bundlemaker.core.internal.parser;
+
+import org.bundlemaker.core.util.StopWatch;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class StaticLog {
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param message
+   */
+  public static void log(String message) {
+    System.out.println(message);
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param message
+   * @param args
+   */
+  public static void log(String message, Object... args) {
+    System.out.println(String.format(message, args));
+  }
+
+  public static <T> T log(boolean log, String message, LoggableAction<T> loggableAction) {
+
+    //
+    StopWatch stopWatch = null;
+
+    //
+    if (log) {
+      StaticLog.log("Starting '%s'...", message);
+      stopWatch = new StopWatch();
+      stopWatch.start();
+    }
+
+    T result = loggableAction.execute();
+
+    //
+    if (log) {
+      StaticLog.log("Finished '%s' [%s ms]", message, stopWatch.getElapsedTime());
+    }
+
+    return result;
+  }
+}
