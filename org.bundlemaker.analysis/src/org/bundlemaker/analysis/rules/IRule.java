545817db416df9912a1872de55e973b87b341a9c
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-07-12 14:46:19 +0200

    test

--- org.bundlemaker.analysis/src/org/bundlemaker/analysis/rules/IRule.java
+++ org.bundlemaker.analysis/src/org/bundlemaker/analysis/rules/IRule.java
@@ -3,29 +3,29 @@ package org.bundlemaker.analysis.rules;
 import org.bundlemaker.analysis.model.IDependency;
 
 /**
- * <p>Das Interface einer Regel
- * 
- * <p>Regeln sind von der Architektur vorgegebene Ordnungen, die
- * bei nicht Einhaltung einen Regelverstoﬂ hervorbringen
+ * <p>
+ * Defines an architecture rule.
+ * </p>
  * 
  * @author Kai Lehmann
- *
+ * 
  */
 public interface IRule {
 
-	
-	public void setName(String name);
-	
-	public String getName();
+  public void setName(String name);
 
-	/**
-	 * Prueft, ob die uebergebene Abhaengigkeit gegen die Regel verstoeﬂt
-	 * @param dependency die Abhaengigkeit
-	 * @return <code>true</bode>, falls die Abhaengigkeit gegen die Regel verstoest. 
-	 * Ansonsten <code>false</code>
-	 */
-	public boolean isViolatedBy(IDependency dependency);
-	
-	public Violation getViolation();
-	
+  public String getName();
+
+  /**
+   * Prueft, ob die uebergebene Abhaengigkeit gegen die Regel verstoeﬂt
+   * 
+   * @param dependency
+   *          die Abhaengigkeit
+   * @return <code>true</bode>, falls die Abhaengigkeit gegen die Regel verstoest. 
+   * Ansonsten <code>false</code>
+   */
+  public boolean isViolatedBy(IDependency dependency);
+
+  public Violation getViolation();
+
 }
