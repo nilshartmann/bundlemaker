f4d3bcf65132efadbe7bac2377eef1c2f734af1f
Nils Hartmann <Nils.Hartmann@tk.de>
2011-07-18 11:19:28 +0200

    Javadoc

--- main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/Analysis.java
+++ main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/Analysis.java
@@ -5,12 +5,26 @@ import org.bundlemaker.analysis.ui.internal.Activator;
 import org.bundlemaker.analysis.ui.internal.AnalysisContext;
 import org.eclipse.swt.graphics.Image;
 
+/**
+ * Common functionality used by BundleMaker analysis features
+ * 
+ * @author Nils Hartmann
+ * 
+ */
 public class Analysis {
 
   private static Analysis        _instance;
 
   private final IAnalysisContext _analysisContext;
 
+  /**
+   * Returns the singleton instnace of analysis.
+   * 
+   * <p>
+   * There is exactly one Analysis instance per Eclispe instance
+   * 
+   * @return
+   */
   public static Analysis instance() {
     if (_instance == null) {
       _instance = new Analysis();
@@ -19,14 +33,31 @@ public class Analysis {
     return _instance;
   }
 
+  /**
+   * Use {@link #instance()} to retrieve the singleton instance of this class
+   */
   private Analysis() {
     _analysisContext = new AnalysisContext();
   }
 
+  /**
+   * Returns the {@link IAnalysisContext} instance
+   * 
+   * @return
+   */
   public IAnalysisContext getContext() {
     return _analysisContext;
   }
 
+  /**
+   * Returns the Icon-Image that should be used for the given artifact.
+   * 
+   * <p>
+   * If no artifact is given, <tt>null</tt> is returned.
+   * 
+   * @param artifact
+   * @return
+   */
   public Image getIconForArtifact(IArtifact artifact) {
     if (artifact == null) {
       return null;
