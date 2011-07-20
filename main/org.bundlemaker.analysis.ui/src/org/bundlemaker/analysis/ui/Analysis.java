345ae4a355cafaa3068895c332f03e4807616736
Nils Hartmann <Nils.Hartmann@tk.de>
2011-07-20 12:36:48 +0200

    Add UI Util-Methods to open Editors

--- main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/Analysis.java
+++ main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/Analysis.java
@@ -1,14 +1,18 @@
 package org.bundlemaker.analysis.ui;
 
 import org.bundlemaker.analysis.model.IArtifact;
+import org.bundlemaker.analysis.ui.editor.DependencyPart;
+import org.bundlemaker.analysis.ui.editor.GenericEditor;
 import org.bundlemaker.analysis.ui.internal.Activator;
 import org.bundlemaker.analysis.ui.internal.AnalysisContext;
 import org.bundlemaker.analysis.ui.view.table.DependencyTreeTableView;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.part.NullEditorInput;
 
 /**
  * Common functionality used by BundleMaker UI analysis features
@@ -21,6 +25,9 @@ public class Analysis {
   private static Analysis        _instance;
 
   private final IAnalysisContext _analysisContext;
+
+  @SuppressWarnings("restriction")
+  private IEditorInput           nullInputEditor = new NullEditorInput();
 
   /**
    * Returns the singleton instnace of analysis.
@@ -77,13 +84,12 @@ public class Analysis {
    * @return
    */
   public DependencyTreeTableView getDependencyTreeTableView() {
-    IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-    if (workbenchWindow != null) {
-      IWorkbenchPage workbenchPage = workbenchWindow.getActivePage();
+    IWorkbenchPage workbenchPage = getActiveWorkbenchPage();
+
       if (workbenchPage != null) {
         return (DependencyTreeTableView) workbenchPage.findView(DependencyTreeTableView.ID);
       }
-    }
+
     return null;
   }
 
@@ -91,17 +97,44 @@ public class Analysis {
    * Opens the {@link DependencyTreeTableView}
    */
   public void openDependencyTreeTableView() {
-    try {
-      IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-      if (workbenchWindow != null) {
-        IWorkbenchPage workbenchPage = workbenchWindow.getActivePage();
+    IWorkbenchPage workbenchPage = getActiveWorkbenchPage();
         if (workbenchPage != null) {
+      try {
           workbenchPage.showView(DependencyTreeTableView.ID);
+      } catch (PartInitException e) {
+        e.printStackTrace();
         }
       }
+  }
+
+  /**
+   * Opens the {@link GenericEditor} that is displaying the contributed {@link DependencyPart DependencyParts}
+   */
+  public void openGenericEditor() {
+    IWorkbenchPage page = getActiveWorkbenchPage();
+    if (page != null) {
+      try {
+        page.openEditor(nullInputEditor, GenericEditor.ID);
     } catch (PartInitException e) {
       e.printStackTrace();
     }
+  }
+  }
+
+  /**
+   * Gets the active {@link IWorkbenchPage}.
+   * 
+   * @return the active IWorkbenchPage or null
+   */
+  private IWorkbenchPage getActiveWorkbenchPage() {
+    IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+    if (workbenchWindow != null) {
+      IWorkbenchPage workbenchPage = workbenchWindow.getActivePage();
+      if (workbenchPage != null) {
+        return workbenchPage;
+      }
+    }
+    return null;
   }
 
 }
