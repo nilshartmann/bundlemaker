99fe497758b6fe9039a82889153822e1c966e1f5
Nils Hartmann <nils@nilshartmann.net>
2011-08-06 15:20:26 +0200

    BM-135: Create Listener-API for selected BundleMaker items
    Added javadoc/copyright message

--- main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/internal/selection/ArtifactSelectionService.java
+++ main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/internal/selection/ArtifactSelectionService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Bundlemaker project team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Bundlemaker project team - initial API and implementation
+ ******************************************************************************/
 package org.bundlemaker.analysis.ui.internal.selection;
 
 import java.util.Collection;
@@ -64,12 +74,12 @@ public class ArtifactSelectionService extends
     Assert.isNotNull(listener, "The parameter 'listener' must not be null");
 
     removeSelectionListener(listener);
-    }
+  }
 
-    @Override
+  @Override
   protected void invokeListener(IArtifactSelectionListener listener, IArtifactSelectionChangedEvent event) {
     listener.artifactSelectionChanged(event);
 
-    }
-
   }
+
+}
--- main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/internal/selection/DependencySelectionService.java
+++ main/org.bundlemaker.analysis.ui/src/org/bundlemaker/analysis/ui/internal/selection/DependencySelectionService.java
@@ -1,13 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Bundlemaker project team.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Bundlemaker project team - initial API and implementation
+ ******************************************************************************/
 package org.bundlemaker.analysis.ui.internal.selection;
 
 import java.util.Collection;
 
 import org.bundlemaker.analysis.model.IDependency;
+import org.bundlemaker.analysis.ui.Analysis;
 import org.bundlemaker.analysis.ui.selection.IDependencySelection;
 import org.bundlemaker.analysis.ui.selection.IDependencySelectionChangedEvent;
 import org.bundlemaker.analysis.ui.selection.IDependencySelectionListener;
 import org.bundlemaker.analysis.ui.selection.IDependencySelectionService;
 
+/**
+ * The implementation of the {@link IDependencySelectionListener}.
+ * 
+ * <p>
+ * Instances of the {@link IDependencySelectionListener} can be received via the {@link Analysis} factory class
+ * 
+ * @author Nils Hartmann (nils@nilshartmann.net)
+ * 
+ */
 public class DependencySelectionService extends
     AbstractSelectionService<IDependencySelectionListener, IDependencySelectionChangedEvent> implements
     IDependencySelectionService {
