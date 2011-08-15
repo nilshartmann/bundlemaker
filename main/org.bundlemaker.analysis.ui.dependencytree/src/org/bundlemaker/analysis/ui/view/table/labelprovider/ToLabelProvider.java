61ab2c54a9e56d436b3b738bbed798a8687a6c4a
Nils Hartmann <nils@nilshartmann.net>
2011-08-15 18:23:45 +0200

    Use DefaultArtifactLabelProvider as base for label provider in DependencyTableTreeView

--- main/org.bundlemaker.analysis.ui.dependencytree/src/org/bundlemaker/analysis/ui/view/table/labelprovider/ToLabelProvider.java
+++ main/org.bundlemaker.analysis.ui.dependencytree/src/org/bundlemaker/analysis/ui/view/table/labelprovider/ToLabelProvider.java
@@ -1,27 +1,25 @@
 package org.bundlemaker.analysis.ui.view.table.labelprovider;
 
+import org.bundlemaker.analysis.model.IArtifact;
 import org.bundlemaker.analysis.model.IDependency;
-import org.bundlemaker.analysis.ui.Analysis;
+import org.bundlemaker.analysis.ui.DefaultArtifactLabelProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 public class ToLabelProvider extends ColumnLabelProvider {
+  private final DefaultArtifactLabelProvider _artifactLabelProvider = new DefaultArtifactLabelProvider();
 
   @Override
   public Image getImage(Object element) {
     IDependency dependency = (IDependency) element;
-    // Classification classification = DependencyRoot.getClassification(dependency.getTo());;
-    // if( (classification != null) && (classification.getKind().equals("Type"))) {
-    // return Activator.getDefault().getIcon("type");
-    // } else {
-    return Analysis.instance().getIconForArtifact(dependency.getTo());
-    // }
+    return _artifactLabelProvider.getImage(dependency.getTo());
   }
 
   @Override
   public String getText(Object element) {
     if (element instanceof IDependency) {
-      return ((IDependency) element).getTo().getName();
+      IArtifact to = ((IDependency) element).getTo();
+      return _artifactLabelProvider.getText(to);
     }
     return element == null ? "" : element.toString();//$NON-NLS-1$
   }
