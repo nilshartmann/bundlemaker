50f3cb2d44e436d3fe8ce3ffce75395751c4d57e
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-08-16 09:58:41 +0200

    ~ new expected result

--- unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/analysis/complex/AbstractComplexTest.java
+++ unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/analysis/complex/AbstractComplexTest.java
@@ -2,14 +2,18 @@ package org.bundlemaker.core.itest.analysis.complex;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.InputStream;
+
 import org.bundlemaker.analysis.model.ArtifactType;
 import org.bundlemaker.analysis.model.IArtifact;
 import org.bundlemaker.analysis.model.IDependency;
 import org.bundlemaker.analysis.model.IDependencyModel;
 import org.bundlemaker.core.analysis.ArtifactModelConfiguration;
+import org.bundlemaker.core.analysis.ArtifactUtils;
 import org.bundlemaker.core.analysis.IAdvancedArtifact;
 import org.bundlemaker.core.analysis.ModelTransformer;
 import org.bundlemaker.core.itest.AbstractModularizedSystemTest;
+import org.bundlemaker.core.util.ModuleUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,13 +31,13 @@ public abstract class AbstractComplexTest extends AbstractModularizedSystemTest 
 
   /** - */
   private IArtifact         _jdk16Artifact;
-  
+
   /** - */
   private IArtifact         _group1Artifact;
-  
+
   /** - */
   private IArtifact         _group2Artifact;
-  
+
   /** - */
   private IArtifact         _missingTypesArtifact;
 
@@ -44,6 +48,12 @@ public abstract class AbstractComplexTest extends AbstractModularizedSystemTest 
     // prepare the model
     assertTypeCount(1438);
 
+    // assert the input
+    InputStream inputstream = getClass().getResourceAsStream("results/" + getTestProjectName() + ".txt");
+    assertResult(ModuleUtils.dump(getModularizedSystem().getResourceModule(getTestProjectName(), "1.0.0"),
+        getModularizedSystem()), inputstream, getTestProjectName() + getCurrentTimeStamp());
+
+    //
     IDependencyModel dependencyModel = ModelTransformer.getDependencyModel(getModularizedSystem(),
         ArtifactModelConfiguration.SOURCE_RESOURCES_CONFIGURATION);
     Assert.assertNotNull(dependencyModel);
@@ -51,13 +61,20 @@ public abstract class AbstractComplexTest extends AbstractModularizedSystemTest 
     _rootArtifact = (IAdvancedArtifact) dependencyModel.getRoot();
     Assert.assertNotNull(_rootArtifact);
 
+    // assert the input
+    inputstream = getClass().getResourceAsStream("results/AbstractComplexTest_Input.txt");
+    assertResult(ArtifactUtils.artifactToString(_rootArtifact), inputstream, "AbstractComplexTest_Input"
+        + getCurrentTimeStamp());
+
+    Assert.assertEquals(8275, _rootArtifact.getDependencies().size());
+
     _group1Artifact = getArtifact(_rootArtifact, "group1");
     _group2Artifact = getArtifact(_rootArtifact, "group1|group2");
     _jeditModuleArtifact = getArtifact(_rootArtifact, "group1|group2|jedit_1.0.0");
     _velocityModuleArtifact = getArtifact(_rootArtifact, "velocity_1.5");
     _jdk16Artifact = getArtifact(_rootArtifact, "jdk16_jdk16");
     _missingTypesArtifact = getArtifact(_rootArtifact, "<< Missing Types >>");
-    
+
     assertDependencyWeight(getGroup1Artifact(), getJdkArtifact(), 1904);
     assertDependencyWeight(getVelocityModuleArtifact(), getJdkArtifact(), 4);
   }
@@ -150,7 +167,7 @@ public abstract class AbstractComplexTest extends AbstractModularizedSystemTest 
   /**
    * <p>
    * </p>
-   *
+   * 
    * @param artifact
    * @param count
    */
