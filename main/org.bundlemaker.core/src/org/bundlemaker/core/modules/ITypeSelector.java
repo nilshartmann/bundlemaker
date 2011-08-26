99569b5313556b77b28d6562cbbc5a7f45d1921b
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-08-26 09:58:16 +0200

    ~ clean-up

--- main/org.bundlemaker.core/src/org/bundlemaker/core/modules/ITypeSelector.java
+++ main/org.bundlemaker.core/src/org/bundlemaker/core/modules/ITypeSelector.java
@@ -24,38 +24,7 @@ public interface ITypeSelector {
 
   /**
    * <p>
-   * Must return <code>true</code> if (and only if) this module selector matches all existing {@link IResourceModule
-   * IResourceModules}. The result of this method is used for consistency checks only. It does not affect the result of
-   * method {@link ITypeSelector#selectType(IModule, String, Set)}.
-   * </p>
-   * <p>
-   * In case that this method returns <code>true</code> the result of method {@link ITypeSelector#getSourceModules()} is
-   * ignored.
-   * </p>
-   * 
-   * @return <code>true</code> if (and only if) this module selector matches all existing {@link IResourceModule
-   *         IResourceModules}.
-   */
-  boolean matchesAllModules();
-
-  /**
-   * <p>
-   * Must return the {@link IModuleIdentifier IModuleIdentifiers} of the modules that matches this module selector. The
-   * result of this method is used for consistency checks only. It does not affect the result of method
-   * {@link ITypeSelector#selectType(IModule, String, Set)}.
-   * </p>
-   * <p>
-   * In case that the method {@link ITypeSelector#matchesAllModules()} returns <code>true</code> the result of this
-   * method is ignored.
-   * </p>
-   * 
-   * @return the {@link IModuleIdentifier IModuleIdentifiers} of the modules that matches this module selector.
-   */
-  Set<IModuleIdentifier> getSourceModules();
-
-  /**
-   * <p>
-   * Selects a {@link IType} for the given {@link IModule} from the set of possible implementations. The set of possible
+   * Selects a {@link IType} for the given {@link IResourceModule} from the set of possible types. The set of possible
    * types contains all types in the system with the given fully qualified name.
    * </p>
    * 
