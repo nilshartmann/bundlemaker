db1ed95043c5007eecbe1af0831592d361de2575
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2011-10-04 14:52:02 +0200

    + added AmbiguousTypesTest

---
+++ unittests/org.bundlemaker.core.itest/test-data/AmbiguousTypesTest/src/javax/xml/ws/handler/soap/SOAPMessageContext.java
@@ -0,0 +1,11 @@
+package javax.xml.ws.handler.soap;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class SOAPMessageContext {
+
+}
---
+++ unittests/org.bundlemaker.core.itest/test-data/AmbiguousTypesTest/src/test/Test.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class Test {
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    javax.xml.ws.handler.soap.SOAPMessageContext context = null;
+    System.out.println(context);
+  }
+}
