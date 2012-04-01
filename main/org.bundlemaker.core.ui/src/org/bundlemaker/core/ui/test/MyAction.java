c5c754b1bbdffa125f12c4417f4e66ae921f68e3
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-04-01 23:48:54 +0200

    + added experimental code for doubleclick support

---
+++ main/org.bundlemaker.core.ui/src/org/bundlemaker/core/ui/test/MyAction.java
@@ -0,0 +1,15 @@
+package org.bundlemaker.core.ui.test;
+
+import org.eclipse.jface.action.Action;
+
+public class MyAction extends Action {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void run() {
+    super.run();
+    System.out.println("ASKDHAKSDHKASHDKAHSD");
+  }
+}
