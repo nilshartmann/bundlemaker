9c570d54caba310c1ad3733a43b64218a4d65b06
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-04-03 09:33:19 +0200

    + added new project 'org.bundlemaker.core.ui.app.perspective'

---
+++ main/org.bundlemaker.core.ui.app.perspective/src/org/bundlemaker/core/ui/app/perspective/BundleMakerPerspectiveFactory.java
@@ -0,0 +1,29 @@
+package org.bundlemaker.core.ui.app.perspective;
+
+import org.bundlemaker.core.ui.view.dependencytable.DependencyTableView;
+import org.bundlemaker.core.ui.view.dependencytree.DependencyTreeView;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class BundleMakerPerspectiveFactory implements IPerspectiveFactory {
+
+  @Override
+  public void createInitialLayout(IPageLayout layout) {
+
+    // add wizard shortcut
+    layout.addNewWizardShortcut("org.bundlemaker.core.ui.wizards.newwizard");
+
+    //
+    String editorArea = layout.getEditorArea();
+
+    layout.addView(IPageLayout.ID_PROJECT_EXPLORER, IPageLayout.LEFT, 0.25f, editorArea);
+
+    IFolderLayout middle = layout.createFolder("middle", IPageLayout.BOTTOM, 0.5f, editorArea);
+    middle.addView(DependencyTreeView.ID);
+
+    IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.5f, "middle");
+    bottom.addView(DependencyTableView.ID);
+    bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+  }
+}
