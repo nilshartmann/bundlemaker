1f548a161fc5a365a88a3815d615797f4c200074
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-05-05 11:02:12 +0200

    ~ first draft HTML exporter

---
+++ main/org.bundlemaker.core.reports/src/org/bundlemaker/core/reports/exporter/html/AbstractSingleModuleHtmlReportExporter.java
@@ -0,0 +1,76 @@
+package org.bundlemaker.core.reports.exporter.html;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.bundlemaker.core.exporter.AbstractExporter;
+import org.bundlemaker.core.exporter.IModuleExporter;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public abstract class AbstractSingleModuleHtmlReportExporter extends AbstractExporter implements IModuleExporter {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void doExport(IProgressMonitor progressMonitor) throws CoreException {
+
+    //
+    SubMonitor subMonitor = SubMonitor.convert(progressMonitor, 3);
+    subMonitor.beginTask(null, 3);
+
+    try {
+
+      //
+      File file = new File(getCurrentContext().getDestinationDirectory(), getCurrentModule().getModuleIdentifier()
+          .toString() + ".html");
+
+      if (!file.getParentFile().exists()) {
+        file.getParentFile().mkdirs();
+      }
+
+      //
+      BufferedWriter bufferedWriter = getBufferedWriter(file);
+
+      writeHtmlHead(bufferedWriter);
+
+      bufferedWriter.flush();
+      bufferedWriter.close();
+      subMonitor.worked(2);
+
+    } catch (Exception e) {
+      // TODO
+      e.printStackTrace();
+      throw new CoreException(new Status(IStatus.ERROR, "", ""));
+    }
+
+  }
+
+  private BufferedWriter getBufferedWriter(File file) throws IOException {
+    FileWriter fw = new FileWriter(file);
+    return new BufferedWriter(fw, 8192);
+  }
+
+  private void writeHtmlHead(BufferedWriter bw) throws IOException {
+    bw.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\""
+        + "\"http://www.w3.org/TR/html4/loose.dtd\">\n");
+    bw.write("<html>\n");
+    bw.write("<head>\n");
+    bw.write("  <title>HALLO</title>\n");
+    bw.write("  <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\n");
+    bw.write("  <link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\">\n");
+    bw.write("</head>\n");
+  }
+}
---
+++ main/org.bundlemaker.core.reports/src/org/bundlemaker/core/reports/exporter/html/DependenciesReportExporter.java
@@ -0,0 +1,6 @@
+package org.bundlemaker.core.reports.exporter.html;
+
+public class DependenciesReportExporter extends AbstractSingleModuleHtmlReportExporter {
+
+  
+}
