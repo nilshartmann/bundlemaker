4139ff7a9045a9fd2288eb237dbde0e0499c4ee3
Nils Hartmann <nils@nilshartmann.net>
2012-10-16 19:33:25 +0200

    Select script that should be executed

--- _incubator/org.bundlemaker.core.ui.transformation/src/org/bundlemaker/core/ui/transformation/handlers/RunTransformationScriptHandler.java
+++ _incubator/org.bundlemaker.core.ui.transformation/src/org/bundlemaker/core/ui/transformation/handlers/RunTransformationScriptHandler.java
@@ -1,9 +1,9 @@
 package org.bundlemaker.core.ui.transformation.handlers;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.List;
 
-import org.bundlemaker.core.IBundleMakerProject;
 import org.bundlemaker.core.analysis.IBundleMakerArtifact;
 import org.bundlemaker.core.analysis.IRootArtifact;
 import org.bundlemaker.core.transformations.script.ITransformationScript;
@@ -11,25 +11,32 @@ import org.bundlemaker.core.ui.handler.AbstractArtifactBasedHandler;
 import org.bundlemaker.core.ui.transformation.Activator;
 import org.bundlemaker.core.ui.transformation.console.TransformationScriptConsoleFactory;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeHierarchy;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jdt.ui.dialogs.ITypeInfoFilterExtension;
+import org.eclipse.jdt.ui.dialogs.ITypeInfoRequestor;
+import org.eclipse.jdt.ui.dialogs.TypeSelectionExtension;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.jface.operation.IRunnableContext;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ISelectionStatusValidator;
+import org.eclipse.ui.dialogs.SelectionDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.model.WorkbenchContentProvider;
-import org.eclipse.ui.model.WorkbenchLabelProvider;
-import org.eclipse.ui.views.navigator.ResourceComparator;
+import org.eclipse.ui.progress.IProgressService;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -38,14 +45,13 @@ import org.eclipse.ui.views.navigator.ResourceComparator;
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class RunTransformationScriptHandler extends AbstractArtifactBasedHandler {
+
+  private final static String TRANSFORMATION_SCRIPT_TYPE_NAME = ITransformationScript.class.getName();
+
   /**
    * The constructor.
    */
   public RunTransformationScriptHandler() {
-  }
-
-  protected void doExecute(ExecutionEvent event) throws Exception {
-
   }
 
   /*
@@ -60,13 +66,20 @@ public class RunTransformationScriptHandler extends AbstractArtifactBasedHandler
       return;
     }
 
+    IType transformationScriptType = selectTransformationScript(HandlerUtil.getActiveShell(event));
+
+    if (transformationScriptType == null) {
+      return;
+    }
+
     IBundleMakerArtifact selectedArtifact = selectedArtifacts.get(0);
     final IRootArtifact rootArtifact = selectedArtifact.getRoot();
 
     // selectTransformationScript(rootArtifact.getModularizedSystem().getBundleMakerProject());
 
-    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("transformation-script");
+    IProject project = transformationScriptType.getResource().getProject();
 
+    // TODO parse classpath
     IFolder binFolder = (IFolder) project.findMember("bin");
     if (binFolder == null) {
       IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
@@ -77,15 +90,12 @@ public class RunTransformationScriptHandler extends AbstractArtifactBasedHandler
     String urlString = binFolder.getLocationURI().toURL().toString() + "/";
     URL url = new URL(urlString);
 
-    // URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { url });
-
     TransformationScriptConsoleFactory.showConsole();
 
     TransformationScriptClassLoader classLoader = TransformationScriptClassLoader.createBundleClassLoaderFor(Activator
         .getDefault().getBundle(), new URL[] { url });
-    Class<?> loadClass = classLoader.loadClass("org.bm.transform.TestScript");
+    Class<?> loadClass = classLoader.loadClass(transformationScriptType.getFullyQualifiedName());
     Object object = loadClass.newInstance();
-    System.out.println("object: " + object);
     ITransformationScript transformationScript = (ITransformationScript) object;
     TransformationScriptLogger logger = new TransformationScriptLogger();
     try {
@@ -97,64 +107,87 @@ public class RunTransformationScriptHandler extends AbstractArtifactBasedHandler
     refreshProjectExplorer(rootArtifact);
   }
 
-  /**
-   * Asks the user to select a transformation script from the workspace.
-   * 
-   * <p>
-   * The dialog that will be opened only displays <tt>.bmt</tt>-transformation scripts
-   * 
-   * @param project
-   * @return the selected BMT-file or null if no file has been selected
-   */
-  private IFile selectTransformationScript(IBundleMakerProject project) {
-    ElementTreeSelectionDialog dialog = new ElementTreeSelectionDialog(new Shell(), new WorkbenchLabelProvider(),
-        new WorkbenchContentProvider());
-    dialog.setHelpAvailable(false);
-    dialog.setTitle("Choose transformation script");
-    dialog.setMessage("Choose a transformation script that will be applied on " + project.getProject().getName());
+  private IType selectTransformationScript(final Shell shell) {
 
-    // set the filter that filters bmt-files and their containing directories
-    dialog.addFilter(new ViewerFilter() {
-
+    IRunnableContext runnableContext = new IRunnableContext() {
       @Override
-      public boolean select(Viewer viewer, Object parentElement, Object element) {
-        System.out.println("element: " + element + " javaElement: " + (element instanceof IJavaElement));
-        return true;
-
+      public void run(boolean fork, boolean cancelable, IRunnableWithProgress runnable)
+          throws InvocationTargetException, InterruptedException {
+        IProgressService manager = PlatformUI.getWorkbench().getProgressService();
+        manager.busyCursorWhile(runnable);
       }
-    });
-
-    // set the validator that checks that only IFile instances can be selected
-    dialog.setValidator(new ISelectionStatusValidator() {
-
-      private IStatus fgErrorStatus = new Status(IStatus.ERROR, Activator.PLUGIN_ID, "");
-
-      private IStatus fgOKStatus    = new Status(IStatus.OK, Activator.PLUGIN_ID, "");
-
-      @Override
-      public IStatus validate(Object[] selection) {
-
-        for (Object object : selection) {
-          if (!(object instanceof IFile)) {
-            return fgErrorStatus;
-          }
-        }
-        return fgOKStatus;
-      }
-    });
-    dialog.setInput(ResourcesPlugin.getWorkspace().getRoot());
-    dialog.setComparator(new ResourceComparator(ResourceComparator.NAME));
-    dialog.setAllowMultiple(false);
-
-    // open the dialog and return the selected IFile
-    if (dialog.open() == Window.OK) {
-      Object firstResult = dialog.getFirstResult();
-      if (firstResult == null) {
+    };
+    IJavaSearchScope scope = SearchEngine.createWorkspaceScope();
+    try {
+      SelectionDialog typeDialog = JavaUI.createTypeDialog(shell, runnableContext, scope,
+          IJavaElementSearchConstants.CONSIDER_CLASSES, false, "*TransformationScript",
+          new TransformationScriptTypeDialogExtension());
+      if (typeDialog.open() != Window.OK) {
         return null;
       }
-      IFile selectedScript = (IFile) firstResult;
-      return selectedScript;
+
+      Object[] result = typeDialog.getResult();
+      IType transformationScriptType = (IType) result[0];
+      return transformationScriptType;
+
+    } catch (JavaModelException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
     return null;
   }
+
+  class TransformationScriptTypeDialogExtension extends TypeSelectionExtension {
+    @Override
+    public ISelectionStatusValidator getSelectionValidator() {
+      return new ISelectionStatusValidator() {
+        @Override
+        public IStatus validate(Object[] selection) {
+          if (selection.length == 1) {
+            try {
+              IType type = (IType) selection[0];
+              ITypeHierarchy hierarchy = type.newSupertypeHierarchy(new NullProgressMonitor());
+
+              IType[] types = hierarchy.getAllSuperInterfaces(type);
+              for (IType curr : types) {
+                String fullyQualifiedName = curr.getFullyQualifiedName('.');
+                System.out.println("fullyQualifiedName: " + fullyQualifiedName);
+                if (TRANSFORMATION_SCRIPT_TYPE_NAME.equals(fullyQualifiedName)) { //$NON-NLS-1$
+                  return Status.OK_STATUS;
+                }
+
+              }
+
+            } catch (JavaModelException e) {
+              // TODO
+              e.printStackTrace();
+              return Status.CANCEL_STATUS;
+            }
+          }
+          return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Type is not an instanceof "
+              + TRANSFORMATION_SCRIPT_TYPE_NAME);
+        }
+
+      };
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jdt.ui.dialogs.TypeSelectionExtension#getFilterExtension()
+     */
+    @Override
+    public ITypeInfoFilterExtension getFilterExtension() {
+      return new ITypeInfoFilterExtension() {
+
+        @Override
+        public boolean select(ITypeInfoRequestor typeInfoRequestor) {
+          // exclude classes coming from bundlemaker itself
+          return (!typeInfoRequestor.getPackageName().startsWith("org.bundlemaker.core"));
+        }
+      };
+    }
+
+  }
+
 }
