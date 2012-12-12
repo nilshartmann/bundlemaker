09bfa711f97467626237107e8f34b3597a0e5fff
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-12-12 10:32:09 +0100

    ~ added GSON support in AbstractPatternBasedSelector

--- main/org.bundlemaker.core/src/org/bundlemaker/core/analysis/selectors/AbstractPatternBasedSelector.java
+++ main/org.bundlemaker.core/src/org/bundlemaker/core/analysis/selectors/AbstractPatternBasedSelector.java
@@ -17,32 +17,178 @@ import org.bundlemaker.core.analysis.IBundleMakerArtifact;
 import org.bundlemaker.core.util.PatternUtils;
 import org.eclipse.core.runtime.Assert;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
- * @author Nils Hartmann (nils@nilshartmann.net)
+ * <p>
+ * </p>
  * 
+ * @author Nils Hartmann (nils@nilshartmann.net)
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
 public abstract class AbstractPatternBasedSelector implements IArtifactSelector {
 
+  /** - */
+  @Expose
+  @SerializedName("artifact")
   private final IBundleMakerArtifact _artifact;
 
-  private final Pattern[]            _includePattern;
+  /** - */
+  @Expose
+  @SerializedName("includePattern")
+  private final String               _includePatternsAsString;
 
-  private final Pattern[]            _excludePattern;
+  /** - */
+  @Expose
+  @SerializedName("excludePattern")
+  private final String               _excludePatternsAsString;
 
+  /** - */
+  private Pattern[]                  _includePattern;
+
+  /** - */
+  private Pattern[]                  _excludePattern;
+
+  /** - */
+  private boolean                    _initialized = false;
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link AbstractPatternBasedSelector}.
+   * </p>
+   * 
+   * @param artifact
+   * @param includePattern
+   * @param excludePattern
+   */
   public AbstractPatternBasedSelector(IBundleMakerArtifact artifact, String includePattern, String excludePattern) {
     Assert.isNotNull(artifact);
     _artifact = artifact;
-    //
-    _includePattern = splitAndConvert(includePattern);
-    _excludePattern = splitAndConvert(excludePattern);
 
+    //
+    _includePatternsAsString = includePattern;
+    _excludePatternsAsString = excludePattern;
+
+    //
+    initialize();
   }
 
+  /**
+   * <p>
+   * Creates a new instance of type {@link AbstractPatternBasedSelector}.
+   * </p>
+   * 
+   * @param artifact
+   * @param includePattern
+   */
   public AbstractPatternBasedSelector(IBundleMakerArtifact artifact, String includePattern) {
     this(artifact, includePattern, null);
-
   }
 
+  /**
+   * <p>
+   * </p>
+   * 
+   * @return
+   */
+  protected IBundleMakerArtifact getBundleMakerArtifact() {
+    return this._artifact;
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param packageName
+   * @return
+   */
+  protected boolean isExcluded(String packageName) {
+
+    //
+    initialize();
+
+    if (_excludePattern == null) {
+      // nothing is excluded
+      return false;
+    }
+
+    return matchesAny(packageName, _excludePattern);
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param packageName
+   * @return
+   */
+  protected boolean isIncluded(String packageName) {
+
+    //
+    initialize();
+
+    if (_includePattern == null) {
+      // every package is included
+      return true;
+    }
+
+    return matchesAny(packageName, _includePattern);
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param packageName
+   * @param patterns
+   * @return
+   */
+  protected boolean matchesAny(String packageName, Pattern[] patterns) {
+
+    //
+    initialize();
+
+    //
+    for (Pattern pattern : patterns) {
+      if (pattern.matcher(packageName).matches()) {
+        return true;
+      }
+    }
+
+    //
+    return false;
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param includePattern
+   * @param excludePattern
+   */
+  private void initialize() {
+
+    //
+    if (_initialized) {
+      return;
+    }
+
+    //
+    _includePattern = splitAndConvert(_includePatternsAsString);
+    _excludePattern = splitAndConvert(_excludePatternsAsString);
+
+    //
+    _initialized = true;
+  }
+
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param s
+   * @return
+   */
   private static Pattern[] splitAndConvert(String s) {
     if (s == null) {
       return null;
@@ -59,41 +205,46 @@ public abstract class AbstractPatternBasedSelector implements IArtifactSelector 
 
   }
 
-  protected IBundleMakerArtifact getBundleMakerArtifact() {
-    return this._artifact;
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((_artifact == null) ? 0 : _artifact.hashCode());
+    result = prime * result + ((_excludePatternsAsString == null) ? 0 : _excludePatternsAsString.hashCode());
+    result = prime * result + ((_includePatternsAsString == null) ? 0 : _includePatternsAsString.hashCode());
+    return result;
   }
 
-  protected boolean isExcluded(String packageName) {
-    if (_excludePattern == null) {
-      // nothing is excluded
-      return false;
-    }
-
-    return matchesAny(packageName, _excludePattern);
-  }
-
-  protected boolean isIncluded(String packageName) {
-    if (_includePattern == null) {
-      // every package is included
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
       return true;
-    }
-
-    return matchesAny(packageName, _includePattern);
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    AbstractPatternBasedSelector other = (AbstractPatternBasedSelector) obj;
+    if (_artifact == null) {
+      if (other._artifact != null)
+        return false;
+    } else if (!_artifact.equals(other._artifact))
+      return false;
+    if (_excludePatternsAsString == null) {
+      if (other._excludePatternsAsString != null)
+        return false;
+    } else if (!_excludePatternsAsString.equals(other._excludePatternsAsString))
+      return false;
+    if (_includePatternsAsString == null) {
+      if (other._includePatternsAsString != null)
+        return false;
+    } else if (!_includePatternsAsString.equals(other._includePatternsAsString))
+      return false;
+    return true;
   }
 
-  /**
-   * @param packageName
-   * @param includedPackagesPattern
-   * @return
-   */
-  protected boolean matchesAny(String packageName, Pattern[] patterns) {
-    for (Pattern pattern : patterns) {
-      if (pattern.matcher(packageName).matches()) {
-        return true;
-      }
-    }
-
-    return false;
+  @Override
+  public String toString() {
+    return "AbstractPatternBasedSelector [_artifact=" + _artifact + ", _includePatternsAsString="
+        + _includePatternsAsString + ", _excludePatternsAsString=" + _excludePatternsAsString + "]";
   }
-
 }
--- unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/analysis/simple_artifact_model/AllTests.java
+++ unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/analysis/simple_artifact_model/AllTests.java
@@ -5,12 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ArtifactModelModifiedListenerTest.class, DependenciesFromToTest.class, GetChildByPathTest.class,
-    GroupAddTest.class, GroupCreateNewTest.class, GroupRemoveTest.class, GroupRenameTest.class,
-    InitializeCachesTest.class, MementoTest.class, ModuleChangeClassificationTest.class, ModuleCreateNewTest.class,
-    ModuleRemoveTest.class, ModuleRenameTest.class, TransformationHistoryTest.class,
-    TypeModuleChangeClassificationTest.class, Undo_AddArtifacts_Test.class, Undo_GroupCreateNew_Test.class,
-    Undo_ModuleCreateNew_Test.class, Undo_ModuleRename_Test.class })
+@SuiteClasses({ ArtifactModelModifiedListenerTest.class, ArtifactSelector_addModuleSelectorTest.class,
+    DependenciesFromToTest.class, GetChildByPathTest.class, GroupAddTest.class, GroupCreateNewTest.class,
+    GroupRemoveTest.class, GroupRenameTest.class, InitializeCachesTest.class, MementoTest.class,
+    ModuleChangeClassificationTest.class, ModuleCreateNewTest.class, ModuleRemoveTest.class, ModuleRenameTest.class,
+    TransformationHistoryTest.class, TypeModuleChangeClassificationTest.class, Undo_AddArtifacts_Test.class,
+    Undo_GroupCreateNew_Test.class, Undo_ModuleCreateNew_Test.class, Undo_ModuleRename_Test.class })
 public class AllTests {
 
 }
