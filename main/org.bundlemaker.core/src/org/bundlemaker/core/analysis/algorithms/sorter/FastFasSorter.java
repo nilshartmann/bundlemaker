c2ad52679f6eb1d4a6fcfadfe337682f538cf639
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2012-12-13 15:47:08 +0100

    ~ added 'bubbling'

--- main/org.bundlemaker.core/src/org/bundlemaker/core/analysis/algorithms/sorter/FastFasSorter.java
+++ main/org.bundlemaker.core/src/org/bundlemaker/core/analysis/algorithms/sorter/FastFasSorter.java
@@ -15,22 +15,45 @@ public class FastFasSorter implements IArtifactSorter {
   @Override
   public List<IBundleMakerArtifact> sort(List<IBundleMakerArtifact> artifacts) {
 
-    //
-    List<IBundleMakerArtifact> result = new ArrayList<IBundleMakerArtifact>(artifacts.size());
-
+    // we have to compute the adjacency matrix first
     int[][] adjacencyMatrix = AdjacencyMatrix.computeAdjacencyMatrix(null, artifacts);
 
+    // the ordered sequence (highest first!)
     FastFAS fastFAS = new FastFAS(adjacencyMatrix);
     int[] ordered = fastFAS.getOrderedSequence();
-    for (int i = 1; i <= ordered.length; i++) {
-      result.add(artifacts.get(ordered[ordered.length - i]));
+
+    // Bubbles
+    for (int outerIndex = 1; outerIndex < ordered.length; outerIndex++) {
+      for (int index = 1; index <= outerIndex; index++) {
+
+        //
+        if (adjacencyMatrix[ordered[index]][ordered[index - 1]] > adjacencyMatrix[ordered[index - 1]][ordered[index]]) {
+
+          // swap...
+          int temp = ordered[index];
+          ordered[index] = ordered[index - 1];
+          ordered[index - 1] = temp;
+
+        } else {
+
+          // stop bubbling...
+          break;
+        }
+      }
+    }
+
+    // reverse it
+    ordered = FastFAS.reverse(ordered);
+
+    // create the result list
+    List<IBundleMakerArtifact> result = new ArrayList<IBundleMakerArtifact>(artifacts.size());
+    for (int index : ordered) {
+      result.add(artifacts.get(index));
     }
 
     //
     artifacts.clear();
     artifacts.addAll(result);
-
-    //
     return artifacts;
   }
 }
