887155a0469df8cfdcc720e8662e9d0b11f3f845
Nils Hartmann <nils@nilshartmann.net>
2013-01-31 16:24:23 +0100

    Allow customization of displayed labels

--- main/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/DsmDetailComposite.java
+++ main/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/DsmDetailComposite.java
@@ -1,15 +1,23 @@
 package org.bundlemaker.core.ui.editor.dsm;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
 public class DsmDetailComposite extends Composite {
+	
+	private final Set<PropertyChangeListener> _propertyChangeListeners = new CopyOnWriteArraySet<PropertyChangeListener>();
 
   /** - */
   private Label  _fromLabel;
@@ -25,6 +33,14 @@ public class DsmDetailComposite extends Composite {
 
   /** - */
   private Button _pinSelection;
+  
+  /** - */
+  private Button _qualifiedNamesButton;
+  
+  /** - */
+  private Button _shortendNamesButton;
+  
+  private LabelPresentationMode _labelPresentationMode;
 
   /**
    * <p>
@@ -41,14 +57,16 @@ public class DsmDetailComposite extends Composite {
     Assert.isNotNull(viewWidget);
 
     // this.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_RED));
-    this.setLayout(new FillLayout());
+    GridLayout l = new GridLayout(3, false);
+    this.setLayout(l);
 
     Composite composite = new Composite(this, SWT.NONE);
     GridLayout gridLayout = new GridLayout(2, false);
     gridLayout.marginHeight = 0;
     gridLayout.marginWidth = 0;
     composite.setLayout(gridLayout);
-
+    GridDataFactory.swtDefaults().grab(true, true).align(SWT.FILL, SWT.FILL).applyTo(composite);
+    
     //
     _selectionCountLabel = new Label(composite, SWT.TRAIL);
     GridDataFactory.swtDefaults().grab(false, false).align(SWT.FILL, SWT.FILL).applyTo(_selectionCountLabel);
@@ -66,10 +84,44 @@ public class DsmDetailComposite extends Composite {
     composite.setLayout(gridLayout);
     _visualizeChildren = new Button(composite, SWT.CHECK);
     _visualizeChildren.setText("Visualize children of selected artifacts");
-    
     _pinSelection = new Button(composite, SWT.CHECK);
     _pinSelection.setText("Pin Selection");
+    
+    Composite presentationModeComposite = new Composite(this, SWT.BORDER);
+    gridLayout = new GridLayout(1, false);
+    gridLayout.marginHeight = 0;
+    gridLayout.marginWidth = 0;
+    presentationModeComposite.setLayout(gridLayout);
+    
+    _qualifiedNamesButton = new Button(presentationModeComposite, SWT.CHECK);
+    _qualifiedNamesButton.setText("Show qualified names");
+    _qualifiedNamesButton.setSelection(true);
+    
+    _shortendNamesButton = new Button(presentationModeComposite, SWT.CHECK);
+    _shortendNamesButton.setText("Shorten labels");
+    
+    _qualifiedNamesButton.addSelectionListener(new SelectionAdapter() {
 
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			_shortendNamesButton.setEnabled(_qualifiedNamesButton.getSelection());
+			labelPresentationModeChanged();
+		}
+    	
+	});
+    
+    _shortendNamesButton.addSelectionListener(new SelectionAdapter() {
+
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			labelPresentationModeChanged();
+		}
+    	
+	});
+    
+
+    
+    _labelPresentationMode = determineLabelPresentationMode();
   }
 
   /**
@@ -121,7 +173,8 @@ public class DsmDetailComposite extends Composite {
   public Button getPinSelectionButton() {
     return _pinSelection;
   }
-
+  
+ 
   /**
    * <p>
    * </p>
@@ -143,4 +196,45 @@ public class DsmDetailComposite extends Composite {
     //
     return result;
   }
+
+	public LabelPresentationMode determineLabelPresentationMode() {
+		
+		if (!_qualifiedNamesButton.getSelection()) {
+			return LabelPresentationMode.simpleName;
+		}
+		
+		if (_shortendNamesButton.getSelection()) {
+			return LabelPresentationMode.shortendedQualifiedName;
+		}
+		
+		return LabelPresentationMode.qualifiedName;
+	}
+	
+	protected void labelPresentationModeChanged() {
+		LabelPresentationMode current = _labelPresentationMode;
+		
+		LabelPresentationMode newLabelPresentationMode = determineLabelPresentationMode();
+		
+		if (current != newLabelPresentationMode) {
+			_labelPresentationMode = newLabelPresentationMode;
+			
+			PropertyChangeEvent event = new PropertyChangeEvent(this, "labelPresentationMode", current, newLabelPresentationMode);
+			firePropertyChangeEvent(event);
+		}
+	}
+	
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		_propertyChangeListeners.add(listener);
+	}
+	
+	private void firePropertyChangeEvent(final PropertyChangeEvent event) {
+		for (PropertyChangeListener listener : _propertyChangeListeners) {
+			listener.propertyChange(event);
+			
+		}
+	}
+
+	public LabelPresentationMode getLabelPresentationMode() {
+		return this._labelPresentationMode;
+	}
 }
