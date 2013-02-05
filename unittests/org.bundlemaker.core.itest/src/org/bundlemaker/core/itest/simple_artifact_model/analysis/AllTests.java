8191138745f7257d99cff311d766f6878cc3b5d1
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2013-02-05 22:25:17 +0100

    ~ recreate test suite

--- unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/simple_artifact_model/analysis/AllTests.java
+++ unittests/org.bundlemaker.core.itest/src/org/bundlemaker/core/itest/simple_artifact_model/analysis/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ArtifactNamingTest.class, AddArtifactsToPackage_Test.class, ArtifactModelModifiedListenerTest.class,
+@SuiteClasses({ AddArtifactsToPackage_Test.class, ArtifactModelModifiedListenerTest.class, ArtifactNamingTest.class,
     ArtifactSelector_addModuleSelectorTest.class, DependenciesFromToTest.class, GetChildByPathTest.class,
     GroupAddTest.class, GroupCreateNewTest.class, GroupRemoveTest.class, GroupRenameTest.class,
     InitializeCachesTest.class, MementoTest.class, ModuleChangeClassificationTest.class, ModuleCreateNewTest.class,
