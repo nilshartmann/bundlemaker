e04a696a97ee528c79857cd4ce1d75e29ac4fa83
Nils Hartmann <nils@nilshartmann.net>
2013-03-12 20:31:36 +0100

    Don't set selections to Project Explorer (conflicts with new Stage View)

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/DSMArtifactModelEditor.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/DSMArtifactModelEditor.java
@@ -236,14 +236,14 @@ public class DSMArtifactModelEditor extends AbstractArtifactSelectionAwareEditor
   public void onPartActivated() {
 
     //
-    if (!EMPTY_ARTIFACT_SELECTION.equals(getCurrentArtifactSelection())) {
-      Selection
-          .instance()
-          .getArtifactSelectionService()
-          .setSelection(Selection.MAIN_ARTIFACT_SELECTION_ID, DSM_EDITOR_ID,
-              getCurrentArtifactSelection().getSelectedArtifacts(),
-              getCurrentArtifactSelection().useChildrenOfSelectedArtifacts());
-    }
+//    if (!EMPTY_ARTIFACT_SELECTION.equals(getCurrentArtifactSelection())) {
+//      Selection
+//          .instance()
+//          .getArtifactSelectionService()
+//          .setSelection(Selection.MAIN_ARTIFACT_SELECTION_ID, DSM_EDITOR_ID,
+//              getCurrentArtifactSelection().getSelectedArtifacts(),
+//              getCurrentArtifactSelection().useChildrenOfSelectedArtifacts());
+//    }
   }
 
   /**
