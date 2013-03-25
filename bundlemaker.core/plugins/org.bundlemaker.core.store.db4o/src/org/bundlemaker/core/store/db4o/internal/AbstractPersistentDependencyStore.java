d6d841cc4b20cc9a483fca430b41d85824bea99e
Nils Hartmann <nils@nilshartmann.net>
2013-03-25 14:50:04 +0100

    Minor configuration changes to gain a little performance

--- bundlemaker.core/plugins/org.bundlemaker.core.store.db4o/src/org/bundlemaker/core/store/db4o/internal/AbstractPersistentDependencyStore.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.store.db4o/src/org/bundlemaker/core/store/db4o/internal/AbstractPersistentDependencyStore.java
@@ -68,7 +68,7 @@ public abstract class AbstractPersistentDependencyStore implements IPersistentDe
    * </p>
    */
   public final void init() {
-
+	  
     // create a new configuration
     Configuration configuration = Db4o.newConfiguration();
 
@@ -76,17 +76,29 @@ public abstract class AbstractPersistentDependencyStore implements IPersistentDe
     configuration.objectClass(Resource.class).cascadeOnUpdate(true);
     configuration.objectClass(Reference.class).cascadeOnUpdate(true);
     configuration.objectClass(ResourceKey.class).cascadeOnUpdate(true);
-
+    
     // set cascade on activation
     configuration.objectClass(Resource.class).cascadeOnActivate(true);
     configuration.objectClass(Reference.class).cascadeOnActivate(true);
     configuration.objectClass(ResourceKey.class).cascadeOnActivate(true);
-
+    
+    // note: cascadeOnDelete is not possible, otherwise we clean up FlyWeightCache instances!
+    configuration.objectClass(Resource.class).cascadeOnDelete(false);
+    configuration.objectClass(Reference.class).cascadeOnDelete(false);
+    configuration.objectClass(ResourceKey.class).cascadeOnDelete(false);
+    
+    configuration.detectSchemaChanges(false);
+    configuration.callbacks(false);
+    
     // set the activation depth
-    configuration.activationDepth(10);
-
+    // since we really want the whole db in memory, set the activation
+    // depth very high
+    configuration.activationDepth(Integer.MAX_VALUE);
+    
     // open file
-    new File(_fileName).getParentFile().mkdirs();
+    File file = new File(_fileName);
+    
+	file.getParentFile().mkdirs();
     _database = _db4oService.openFile(configuration, _fileName);
 
     // set initialized
@@ -127,6 +139,7 @@ public abstract class AbstractPersistentDependencyStore implements IPersistentDe
   public final void commit() {
     Assert.isTrue(isInitialized(), "The persistent dependency store has not been initialized.");
 
+    
     // commits the database
     _database.commit();
   }
