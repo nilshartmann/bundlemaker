42bad6a9c764a3f3f34e3edb2bd8cc08eb43d773
Nils Hartmann <nils@nilshartmann.net>
2013-03-25 22:23:32 +0100

    unused variable

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.experimental/src/org/bundlemaker/core/ui/experimental/dependencytable/threeway/XRefTreeArtifactLabelProvider.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.experimental/src/org/bundlemaker/core/ui/experimental/dependencytable/threeway/XRefTreeArtifactLabelProvider.java
@@ -6,15 +6,10 @@ import java.util.Set;
 import org.bundlemaker.core.analysis.IBundleMakerArtifact;
 import org.bundlemaker.core.analysis.IRootArtifact;
 import org.bundlemaker.core.ui.artifact.tree.ArtifactTreeLabelProvider;
-import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.jface.viewers.ITableColorProvider;
-import org.eclipse.jface.viewers.ITableFontProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.operations.RedoActionHandler;
 
 /**
  * <p>
@@ -23,8 +18,6 @@ import org.eclipse.ui.operations.RedoActionHandler;
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
 public class XRefTreeArtifactLabelProvider extends ArtifactTreeLabelProvider implements IColorProvider {
-
-  private FontRegistry              registry = new FontRegistry();
 
   /** - */
   private Set<IBundleMakerArtifact> _bundleMakerArtifacts;
@@ -44,10 +37,10 @@ public class XRefTreeArtifactLabelProvider extends ArtifactTreeLabelProvider imp
 
     //
     else {
-      
+
       //
       _bundleMakerArtifacts = new HashSet<IBundleMakerArtifact>(bundleMakerArtifacts);
-      
+
       //
       for (IBundleMakerArtifact artifact : bundleMakerArtifacts) {
         _bundleMakerArtifacts.addAll(artifact.getAncestors());
