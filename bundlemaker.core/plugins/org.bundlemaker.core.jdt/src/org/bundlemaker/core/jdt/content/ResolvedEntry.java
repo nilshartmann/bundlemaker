c7a2b829dfd7801b00077af14672367f6f618c28
Nils Hartmann <nils@nilshartmann.net>
2013-04-03 13:47:57 +0200

    Make sure, each resolved classpath entry is contained almost once in the result. Order result by bringing selected projects to the top.

--- bundlemaker.core/plugins/org.bundlemaker.core.jdt/src/org/bundlemaker/core/jdt/content/ResolvedEntry.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.jdt/src/org/bundlemaker/core/jdt/content/ResolvedEntry.java
@@ -14,106 +14,140 @@ import org.eclipse.core.runtime.IPath;
  */
 public class ResolvedEntry {
 
-  /** - */
-  private IPath       _binaryPath;
+	/** - */
+	private IPath _binaryPath;
 
-  /** - */
-  private List<IPath> _sources;
+	/** - */
+	private List<IPath> _sources;
 
-  /** - */
-  private boolean     _analyze;
+	/** - */
+	private boolean _analyze;
 
-  /** - */
-  private String      _projectName;
+	/** - */
+	private String _projectName;
 
-  /**
-   * <p>
-   * Creates a new instance of type {@link ResolvedEntry}.
-   * </p>
-   * 
-   * @param binaryPath
-   */
-  public ResolvedEntry(IPath binaryPath) {
+	/**
+	 * <p>
+	 * Creates a new instance of type {@link ResolvedEntry}.
+	 * </p>
+	 * 
+	 * @param binaryPath
+	 */
+	public ResolvedEntry(IPath binaryPath) {
 
-    //
-    Assert.isNotNull(binaryPath);
+		//
+		Assert.isNotNull(binaryPath);
 
-    //
-    _binaryPath = binaryPath;
-  }
+		//
+		_binaryPath = binaryPath;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @return
-   */
-  public IPath getBinaryPath() {
-    return _binaryPath;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public IPath getBinaryPath() {
+		return _binaryPath;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @return
-   */
-  public List<IPath> getSources() {
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public List<IPath> getSources() {
 
-    if (_sources == null) {
-      _sources = new LinkedList<IPath>();
-    }
+		if (_sources == null) {
+			_sources = new LinkedList<IPath>();
+		}
 
-    return _sources;
-  }
+		return _sources;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @param analyze
-   */
-  public void setAnalyze(boolean analyze) {
-    _analyze = analyze;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @param analyze
+	 */
+	public void setAnalyze(boolean analyze) {
+		_analyze = analyze;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @return
-   */
-  public boolean isAnalyze() {
-    return _analyze;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public boolean isAnalyze() {
+		return _analyze;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @return
-   */
-  public String getProjectName() {
-    return _projectName;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public String getProjectName() {
+		return _projectName;
+	}
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @param projectName
-   */
-  public void setProjectName(String projectName) {
-    _projectName = projectName;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @param projectName
+	 */
+	public void setProjectName(String projectName) {
+		_projectName = projectName;
+	}
 
-  /**
-   * <p>
-   * </p>
-   *
-   * @return
-   */
-  public boolean hasProjectName() {
-    return _projectName != null;
-  }
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public boolean hasProjectName() {
+		return _projectName != null;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((_binaryPath == null) ? 0 : _binaryPath.hashCode());
+		result = prime * result
+				+ ((_projectName == null) ? 0 : _projectName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResolvedEntry other = (ResolvedEntry) obj;
+		if (_binaryPath == null) {
+			if (other._binaryPath != null)
+				return false;
+		} else if (!_binaryPath.equals(other._binaryPath))
+			return false;
+		if (_projectName == null) {
+			if (other._projectName != null)
+				return false;
+		} else if (!_projectName.equals(other._projectName))
+			return false;
+		return true;
+	}
+
 }
