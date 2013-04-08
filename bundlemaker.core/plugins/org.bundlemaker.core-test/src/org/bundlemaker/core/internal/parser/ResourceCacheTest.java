c2666982c1a9e3698ffff6656749080a30782013
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2013-04-09 00:19:17 +0200

    + added mockito test

---
+++ bundlemaker.core/plugins/org.bundlemaker.core-test/src/org/bundlemaker/core/internal/parser/ResourceCacheTest.java
@@ -0,0 +1,34 @@
+package org.bundlemaker.core.internal.parser;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.bundlemaker.core.internal.store.IPersistentDependencyStore;
+import org.bundlemaker.core.resource.ResourceKey;
+import org.bundlemaker.core.resource.modifiable.IModifiableResource;
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Test;
+
+public class ResourceCacheTest {
+
+  @Test
+  public void testProjectDescription() throws CoreException {
+
+    //
+    IPersistentDependencyStore dependencyStore = mock(IPersistentDependencyStore.class);
+
+    //
+    ResourceCache resourceCache = new ResourceCache(dependencyStore);
+    IModifiableResource modifiableResource = resourceCache.getOrCreateResource(new ResourceKey("1", "12", "123"));
+    
+    //
+    resourceCache.commit(null);
+
+    //
+    verify(dependencyStore, times(1)).updateResource(modifiableResource);
+    
+    //
+    System.out.println(modifiableResource);
+  }
+}
