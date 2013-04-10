cbdfac794be0560514edfe19931e62a575562e3a
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2013-04-10 21:31:36 +0200

    [BM-356] Don't open the source viewer if no source is available

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.artifact/src/org/bundlemaker/core/ui/artifact/tree/EditorHelper.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.artifact/src/org/bundlemaker/core/ui/artifact/tree/EditorHelper.java
@@ -3,7 +3,7 @@ package org.bundlemaker.core.ui.artifact.tree;
 import org.bundlemaker.core.analysis.IResourceArtifact;
 import org.bundlemaker.core.ui.artifact.CommonNavigatorUtils;
 import org.bundlemaker.core.ui.artifact.cnf.ResourceArtifactEditorInput;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
@@ -11,10 +11,35 @@ import org.eclipse.ui.PlatformUI;
 
 public class EditorHelper {
 
+  /**
+   * <p>
+   * </p>
+   * 
+   * @param resourceArtifact
+   */
   public static void openArtifactInEditor(IResourceArtifact resourceArtifact) {
 
+    // return immediately if resource artifact is null
+    if (resourceArtifact == null) {
+      return;
+    }
+
+    // show warning and return immediately if resource artifact has not attached source
+    if (!resourceArtifact.hasAssociatedSourceResource()) {
+
+      //
+      MessageDialog.openWarning(PlatformUI.getWorkbench().getDisplay().getActiveShell(),
+          "No source available.",
+          String.format("The selected artifact '%s' has no attached source.", resourceArtifact.getName()));
+
+      //
+      return;
+    }
+
+    // open the editor part
     try {
-      final IEditorPart editorPart = getActiveWorkbenchPage().openEditor(
+
+      getActiveWorkbenchPage().openEditor(
           new ResourceArtifactEditorInput(resourceArtifact),
           "org.bundlemaker.core.ui.editors.sourceViewerEditor");
 
