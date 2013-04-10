d9c5c76c5fcf919d2dfefc5ead1e2485cd3e8759
Nils Hartmann <nils@nilshartmann.net>
2013-04-10 22:06:58 +0200

    Use different color and font to highlight referenced artifacts

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.experimental/src/org/bundlemaker/core/ui/experimental/dependencytable/threeway/XRefTreeArtifactLabelProvider.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.experimental/src/org/bundlemaker/core/ui/experimental/dependencytable/threeway/XRefTreeArtifactLabelProvider.java
@@ -5,10 +5,13 @@ import java.util.Set;
 
 import org.bundlemaker.core.analysis.IBundleMakerArtifact;
 import org.bundlemaker.core.ui.artifact.tree.ArtifactTreeLabelProvider;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
 
 /**
@@ -17,7 +20,7 @@ import org.eclipse.swt.widgets.Display;
  * 
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
-public class XRefTreeArtifactLabelProvider extends ArtifactTreeLabelProvider implements IColorProvider {
+public class XRefTreeArtifactLabelProvider extends ArtifactTreeLabelProvider implements IColorProvider, IFontProvider {
 
   private final TreeViewer          _treeViewer;
 
@@ -47,71 +50,33 @@ public class XRefTreeArtifactLabelProvider extends ArtifactTreeLabelProvider imp
       //
       _bundleMakerArtifacts = new HashSet<IBundleMakerArtifact>(bundleMakerArtifacts);
 
-      //
-      // for (IBundleMakerArtifact artifact : bundleMakerArtifacts) {
-      // _bundleMakerArtifacts.addAll(artifact.getAncestors());
-      // }
     }
   }
 
   @Override
   public Color getForeground(Object element) {
-
-    System.out.println("getForeground " + element);
-
-    // if (_bundleMakerArtifacts != null) {
-    // for (IBundleMakerArtifact a : _bundleMakerArtifacts) {
-    // System.out.println(" - artifact -> " + a);
-    // }
-    // }
-
     if (_bundleMakerArtifacts != null && _bundleMakerArtifacts.contains(element)) {
-      return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+      return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_RED);
     }
-
-    // if (element instanceof IBundleMakerArtifact) {
-    // IBundleMakerArtifact thisArtifact = (IBundleMakerArtifact) element;
-    //
-    // IStructuredSelection selection = (IStructuredSelection) _treeViewer.getSelection();
-    // if (!selection.isEmpty()) {
-    // boolean child = false;
-    // Iterator<?> iterator = selection.iterator();
-    // while (iterator.hasNext()) {
-    // IBundleMakerArtifact artifact = (IBundleMakerArtifact) iterator.next();
-    //
-    // while (artifact != null) {
-    // if (thisArtifact.equals(artifact)) {
-    // child = true;
-    // break;
-    // }
-    // artifact = artifact.getParent();
-    // }
-    // if (child) {
-    // break;
-    // }
-    // }
-    //
-    // if (child) {
-    // return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
-    // }
-    // }
-    //
-    // }
-
-    // //
-    // if (_bundleMakerArtifacts == null || _bundleMakerArtifacts.contains(element) || element instanceof IRootArtifact)
-    // {
-    // return Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
-    // }
-    //
-    // //
-    // return Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
-
-    return Display.getCurrent().getSystemColor(SWT.COLOR_BLACK);
+    return null;
   }
 
   @Override
   public Color getBackground(Object element) {
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.jface.viewers.IFontProvider#getFont(java.lang.Object)
+   */
+  @Override
+  public Font getFont(Object element) {
+    if (_bundleMakerArtifacts != null && _bundleMakerArtifacts.contains(element)) {
+      return JFaceResources.getFontRegistry().getBold(JFaceResources.DIALOG_FONT);
+    }
+
     return null;
   }
 }
