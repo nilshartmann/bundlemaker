7dc07bd05e5a34abeaf7f5629b37b7722321d9ac
Frank Schlüter <P200329@tk.de>
2013-05-13 13:35:51 +0200

    Bugfix Dsm Size Berechnung nicht fuer die Diagonale

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/widget/internal/util/DsmUtils.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.editor.dsm/src/org/bundlemaker/core/ui/editor/dsm/widget/internal/util/DsmUtils.java
@@ -2,54 +2,58 @@ package org.bundlemaker.core.ui.editor.dsm.widget.internal.util;
 
 public class DsmUtils {
 
-  public static String getLongestString(String[][] values) {
+	public static String getLongestString(String[][] values) {
 
-    // create the result
-    String result = "";
+		// create the result
+		String result = "";
 
-    // iterate over all strings
-    for (String[] value : values) {
-      for (String string : value) {
+		// iterate over all strings
+		for (int row = 0; row < values.length; row++) {
+			String[] zeile = values[row];
+			for (int col = 0; col < zeile.length; col++) {
+				if (row != col) {
+					String string = zeile[col];
+					if (string != null) {
 
-        if (string != null) {
+						if (result == null) {
+							result = string;
+						} else if (result.length() < string.length()) {
+							result = string;
+						}
+					}
+				}
+			}
+		}
 
-          if (result == null) {
-            result = string;
-          } else if (result.length() < string.length()) {
-            result = string;
-          }
-        }
-      }
-    }
+		// return the result
+		return result;
+	}
 
-    // return the result
-    return result;
-  }
-
-  /**
+	/**
    * <p>
    * Helper method that returns the longest string from the string array.
    * </p>
-   * 
+	 *
    * @param strings
    *          the string array
-   * @return the longest string from the string array.
-   */
-  public static String getLongestString(String[] strings) {
+	 * @return  the longest string from the string array.
+	 */
+	public static String getLongestString(String[] strings) {
 
-    // create the result
-    String result = null;
+		// create the result
+		String result = null;
 
-    // iterate over all strings
-    for (String string : strings) {
-      if (result == null) {
-        result = string;
-      } else if (result.length() < string.length()) {
-        result = string;
-      }
-    }
+		// iterate over all strings
+		for (String string : strings) {
+			if (result == null) {
+				result = string;
+			} else if (result.length() < string.length()) {
+				result = string;
+			}
+		}
 
-    // return the result
-    return (result == null ? "" : result);
-  }
+		// return the result
+		return (result == null ? "" : result);
+	}
 }
+
