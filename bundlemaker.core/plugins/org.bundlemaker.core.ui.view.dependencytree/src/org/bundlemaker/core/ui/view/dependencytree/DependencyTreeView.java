075f89f87cfcbae6485a6c81b6ab81c62dab623d
Nils Hartmann <nils@nilshartmann.net>
2013-05-14 10:32:12 +0200

    'Show Reference Count' configurable via View's popup menu (default: off)

--- bundlemaker.core/plugins/org.bundlemaker.core.ui.view.dependencytree/src/org/bundlemaker/core/ui/view/dependencytree/DependencyTreeView.java
+++ bundlemaker.core/plugins/org.bundlemaker.core.ui.view.dependencytree/src/org/bundlemaker/core/ui/view/dependencytree/DependencyTreeView.java
@@ -12,7 +12,12 @@ package org.bundlemaker.core.ui.view.dependencytree;
 
 import org.bundlemaker.core.selection.IDependencySelection;
 import org.bundlemaker.core.ui.event.selection.workbench.view.AbstractDependencySelectionAwareViewPart;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IActionBars;
 
 /**
  * <p>
@@ -25,8 +30,17 @@ public class DependencyTreeView extends AbstractDependencySelectionAwareViewPart
   /** - */
   public static final String              ID = DependencyTreeView.class.getName();
 
+  /** Id used to store view settings in preferences */
+  private static final String        VIEW_SETTINGS_SECTION = "DependencyTreeView";
+
+  
   /** - */
   private CropableDependencyTreeComposite _composite;
+  
+  /**
+   * Turn on/off the display of reference count on each node. Warning: might be expensive.
+   */
+  private boolean _showReferenceCount = false;
 
   /**
    * {@inheritDoc}
@@ -35,7 +49,13 @@ public class DependencyTreeView extends AbstractDependencySelectionAwareViewPart
   public void createPartControl(Composite parent) {
 
     //
-    _composite = new CropableDependencyTreeComposite(parent, ID, false, true);
+    _composite = new CropableDependencyTreeComposite(parent, ID, isShowReferenceCount(), true);
+    
+    // Restore User-made settings
+    loadViewSettings();
+    
+    // Setup Actions and Menu in Action Bar
+    contributeToActionBars();
 
     //
     initDependencies(getCurrentDependencySelection());
@@ -47,6 +67,16 @@ public class DependencyTreeView extends AbstractDependencySelectionAwareViewPart
   @Override
   public void setFocus() {
     //
+  }
+  
+  public boolean isShowReferenceCount() {
+    return _showReferenceCount;
+  }
+  
+  public void setShowReferenceCount(boolean showReferenceCount) {
+    _showReferenceCount = showReferenceCount;
+      _composite.setShowReferenceCount(this._showReferenceCount);
+      saveViewSettings();
   }
 
   /**
@@ -86,5 +116,56 @@ public class DependencyTreeView extends AbstractDependencySelectionAwareViewPart
       _composite.setDependencies(selection.getSelectedDependencies());
     }
   }
+  
+  private void contributeToActionBars() {
+    IActionBars bars = getViewSite().getActionBars();
+    
+    // Pull Down Menu
+    fillLocalPullDown(bars.getMenuManager());
+  }
+  
+  private void fillLocalPullDown(IMenuManager menuManager) {
+    menuManager.add(new ShowReferenceCountAction());
+  }
+  
+  private IDialogSettings getViewSettings() {
+    IDialogSettings settings = Activator.getDefault().getDialogSettings();
+    IDialogSettings section = settings.getSection(VIEW_SETTINGS_SECTION);
+    if (section == null) {
+      section = settings.addNewSection(VIEW_SETTINGS_SECTION);
+    }
+    return section;
+  }
+  
+  private void saveViewSettings() {
+    IDialogSettings dialogSettings = getViewSettings();
+
+    dialogSettings.put("showReferenceCount", isShowReferenceCount());
+  }
+  
+  private void loadViewSettings() {
+    IDialogSettings dialogSettings = getViewSettings();
+
+    this._showReferenceCount = dialogSettings.getBoolean("showReferenceCount");
+    
+    _composite.setShowReferenceCount(_showReferenceCount);
+
+  }
+
+
+  class ShowReferenceCountAction extends Action {
+
+    public ShowReferenceCountAction() {
+      super("Show Reference Count", IAction.AS_CHECK_BOX);
+      setToolTipText("Show Reference Count on each Node. Warning: Might slow down the Tree.");
+      setChecked(isShowReferenceCount());
+    }
+    
+    @Override
+    public void run() {
+      setShowReferenceCount(isChecked());
+    }
+  }  
+  
 
 }
