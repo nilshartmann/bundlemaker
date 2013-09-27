d2a0795595912fbfc0309f2ee69eb0f48fabd8d1
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2013-09-27 21:07:25 +0200

    + added some javadoc

--- bundlemaker.core/plugins/org.bundlemaker.core/src/org/bundlemaker/core/common/classloading/CompoundClassLoader.java
+++ bundlemaker.core/plugins/org.bundlemaker.core/src/org/bundlemaker/core/common/classloading/CompoundClassLoader.java
@@ -3,22 +3,42 @@ package org.bundlemaker.core.common.classloading;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.core.runtime.Assert;
 
 /**
  * <p>
+ * The {@link CompoundClassLoader} contains a list of {@link BundleDelegatingClassLoader BundleDelegatingClassLoaders}
+ * that are traversed to find a requested class.
  * </p>
  * 
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
 public class CompoundClassLoader extends ClassLoader {
 
-  /** - */
+  /** the list of class loaders */
   private List<BundleDelegatingClassLoader> _classLoaders = new LinkedList<BundleDelegatingClassLoader>();
 
-  public boolean add(BundleDelegatingClassLoader e) {
-    return _classLoaders.add(e);
+  /**
+   * <p>
+   * Adds the given class loader to this compound class loader.
+   * </p>
+   * 
+   * @param classLoader
+   *          the class loader
+   * @return <code>true</code> if this collection changed as a result of the call
+   */
+  public boolean add(BundleDelegatingClassLoader classLoader) {
+
+    Assert.isNotNull(classLoader);
+
+    return _classLoaders.add(classLoader);
   }
 
+  /**
+   * <p>
+   * </p>
+   * 
+   */
   public void clear() {
     _classLoaders.clear();
   }
@@ -34,11 +54,11 @@ public class CompoundClassLoader extends ClassLoader {
       try {
         return delegatingClassLoader.findClass(name);
       } catch (Exception e) {
+        // do nothing
       }
     }
 
-    System.err.println("NOT FOUND " + name);
-
+    // delegate to super.findClass()
     return super.findClass(name);
   }
 }
